2fed7a557560e7adbd2efa8a8341470666b0c4fe
gaurisharma360 <gaurisharma360@gmail.com>
2025-10-14 14:43:06 +0200

    updates

--- Data_Preprocessing.R
+++ Data_Preprocessing.R
@@ -2,8 +2,7 @@ library(tidyverse)
 library(lubridate)
 
 # Set path to CSV data file 
-file_path <- "C:/Users/20221564/Data_Science_and_Artificial_Intelligence/Survival-Analysis-DirtSlurper3100-GA/DirtSlurper3100.csv"
-
+#file_path <- "D:\\TUE Study Material\\Q1\\Survival Analysis for Data Scientists\\GA_17\\Survival-Analysis-DirtSlurper3100-GA\\DirtSlurper3100.csv"
 # Load packages 
 
 # Read data
